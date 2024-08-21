40d2855ebc8827905f527b60b4f5554527db63bf
MarcoSac1 <marcosaccone8@gmail.com>
2024-08-21 16:28:09 +0200

    locked on issue

--- vite-boolfolio/src/components/AppMain.vue
+++ vite-boolfolio/src/components/AppMain.vue
@@ -1,16 +1,41 @@
 <script>
-    export default{
-        data(){
-            return{
+import axios, { Axios } from 'axios';
 
-            }
+export default{
+    data(){
+        return{
+            posts:[],
         }
+    },
+    methods:{
+        getPosts(){
+            axios.get('http://127.0.0.1:8000/api/posts', {
+            params: {
+            
+            }
+        })
+        .then((response)=> {
+            console.log(response.data.results.data);
+            this.posts = response.data.results.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });  
+        }
+    },
+    created(){
+        this.getPosts();
     }
+}
 </script>
 
 <template>
     <h1>body</h1>
-
+    <ul>
+        <li v-for="post in posts">
+            {{ post.title }}
+        </li>
+    </ul>
 </template>
 
 
